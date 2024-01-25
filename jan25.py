baaa8ad2a5153702f5e2a0cb5f63b6a5cb7165fb
SnehaaSinghh9 <101444040+SnehaaSinghh9@users.noreply.github.com>
2024-01-25 23:33:10 +0530

    1143 - Medium level

--- jan25.py
+++ jan25.py
@@ -3,7 +3,7 @@
 #Given two strings text1 and text2, return the length of their longest common subsequence. 
 #If there is no common subsequence, return 0.
 #A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
-#For example, "ace" is a subsequence of "abcde".
+#For example, "ace" is a subsequence of "abcde"
 #A common subsequence of two strings is a subsequence that is common to both strings.
 
 class Solution:
